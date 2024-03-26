172ef3af09fb80bdf9497da0a690b73fde303cab
priyanshu082 <priyanshusingh216@gmail.com>
2024-03-26 09:13:28 +0530

    some chnges

--- dbConfig/dbConfig.js
+++ dbConfig/dbConfig.js
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const connectDB =async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        const connection=mongoose.connection
+// const connectDB =async ()=>{
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI)
+//         const connection=mongoose.connection
 
-        connection.on('error',(err)=>{
-            console.log('connection error'+err)
-            process.exit();
-        })
-    } catch (error) {
-        console.log("something went wrong"+error)
-    }
-}
+//         connection.on('error',(err)=>{
+//             console.log('connection error'+err)
+//             process.exit();
+//         })
+//     } catch (error) {
+//         console.log("something went wrong"+error)
+//     }
+// }
 
-export default connectDB
+// export default connectDB
